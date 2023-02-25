cdcfad83985a0bd7d81e79b218c4ce544d624f5a
Angel <aavila002@citymail.cuny.edu>
2023-02-25 17:52:59 -0500

    Navbar created w/ logo and name

---
+++ codepathProject1/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const  Navbar = () => {
+    return (
+        <div className='navbar'>
+            <img src='./src/assets/five-star-hotel-icon.webp' alt='hotel icon' className='hotel-icon' />
+            <p>HotelMania</p>
+        </div>
+    )
+}
+
+export default Navbar
